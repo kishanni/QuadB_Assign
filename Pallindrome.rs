4951ae0aae795a85fe747ba5eecb27a4c8ff21da
kishanni <142335865+kishanni@users.noreply.github.com>
2024-06-29 20:52:08 +0530

    Assignment

---
+++ Pallindrome.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn is_palindrome(s: &str) -> bool {
+    let s = s.chars().filter(|c| c.is_alphanumeric()).collect::<Vec<_>>();
+    s.iter().eq(s.iter().rev())
+}
+
+fn main() {
+    let mut input = String::new();
+    
+    println!("Enter a string to check if it is a palindrome:");
+    
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    
+    let input = input.trim(); // Remove any trailing newline characters
+    
+    if is_palindrome(input) {
+        println!("'{}' is a palindrome.", input);
+    } else {
+        println!("'{}' is not a palindrome.", input);
+    }
+}
